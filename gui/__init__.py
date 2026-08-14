3ff907d341601afab4bf8cd7107cf53a251200ce
awolfman <35307733+awolfman@users.noreply.github.com>
2026-08-14 19:34:45 +0300

    Add files via upload
    Init

---
+++ gui/__init__.py
@@ -0,0 +1,5 @@
+from .main_window import MainWindow
+from .connection_panel import ConnectionPanel
+from .device_panel import DevicePanel
+from .register_editor import RegisterEditor
+__all__ = ['MainWindow', 'ConnectionPanel', 'DevicePanel', 'RegisterEditor']
